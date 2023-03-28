375f98ee8f1baea2d5043e60d9a6bbaff46e9b31
milamgm <nocturnetheater@gmail.com>
2023-03-28 15:42:41 +0200

    feat(router): add router

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -1,17 +1,23 @@
 import "../common/styles/App.scss";
-import { Routes, Route } from 'react-router-dom'
-import { Header, Home } from "../common/utilities";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Episodes, Home, Layout, Podcast } from "../common/utilities";
+import Episode from "../pages/Podcast/Outlet/Episode";
 
 function App() {
-
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/podcast/:podcastId" element={<Podcast />}>
+            <Route index path="*" element={<Episodes />} />
+            <Route path="episode/:episodeId" element={<Episode />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
