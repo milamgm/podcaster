8dec1bbd2ccb2aca5eec0201e5cbd0dacc50913e
milamgm <nocturnetheater@gmail.com>
2023-03-28 16:05:27 +0200

    feat(ItemCard): add ItemCard component and style

--- src/common/components/ItemCard/ItemCard.tsx
+++ src/common/components/ItemCard/ItemCard.tsx
@@ -1,17 +1,21 @@
 import "./ItemCard.scss";
-const ItemCard = () => {
+
+interface Props {
+  id: number;
+  title: string;
+  author: string;
+  image: string;
+}
+
+const ItemCard = ({ title, author, image }: Props) => {
   return (
     <div className="item_card">
       <div className="background">
         <div className="content">
-          <img
-            className="thumbnail"
-            src="https://cdn.pixabay.com/photo/2017/01/17/14/44/pixabay-1987090_1280.png"
-            alt=""
-          />
+          <img className="thumbnail" src={image} alt="" />
           <div className="text">
-            <h4 className="title">All songs considered</h4>
-            <p className="description">Author: NPR</p>
+            <h4 className="title">{title}</h4>
+            <p className="description">{author}</p>
           </div>
         </div>
       </div>
