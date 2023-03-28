5a2d9aa0140992adac32217a91435a98faa577c1
milamgm <nocturnetheater@gmail.com>
2023-03-28 15:54:03 +0200

    feat(utilities): add utilities.tsx file

--- src/common/utilities/index.ts
+++ src/common/utilities/index.ts
@@ -1,8 +1,22 @@
-import Header from "../../application/components/header";
+import retrieveData from "../../application/api/retrieveData";
+import Layout from "../../application/components/Layout";
 import Home from "../../pages/Home";
+import Episodes from "../../pages/Podcast/Outlet/Episodes";
+import ExtendedItemCard from "../components/ExtendedItemCard";
 import ItemCard from "../components/ItemCard";
-export{
-    Home,
-    Header,
-    ItemCard
-}
+import AudioPlayer from "../components/AudioPlayer";
+import Podcast from "../../pages/Podcast";
+import SearchField from "../components/SearchField";
+
+
+export {
+  retrieveData,
+  Layout,
+  Home,
+  Podcast,
+  Episodes,
+  ItemCard,
+  SearchField,
+  ExtendedItemCard,
+  AudioPlayer,
+};
