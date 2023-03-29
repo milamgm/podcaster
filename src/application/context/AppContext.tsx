024b1b67348c72a96027ac020c24cb53f3e8fcac
milamgm <nocturnetheater@gmail.com>
2023-03-29 22:20:45 +0200

    feat(context): add AppContext

--- src/application/context/AppContext.tsx
+++ src/application/context/AppContext.tsx
@@ -1,17 +1,30 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { IAppContext } from "../../common/types";
+import { useRetrieveData } from "../../common/utilities/";
 
 interface Props {
   children: JSX.Element;
 }
 
-const AppContext = createContext({});
+const AppContext = createContext({} as IAppContext);
 
 export const useAppContext = () => {
   return useContext(AppContext);
 };
 
 const AppProvider = ({ children }: Props) => {
-  const values = {};
+  const {data, isLoading} = useRetrieveData();
+  const [podcastsFetch, setPodcastsFetch] = useState([]);
+  const [displayPodcasts, setDisplayPodcasts] = useState([]);
+  const values = { podcastsFetch, displayPodcasts, setDisplayPodcasts, isLoading };
+
+  useEffect(() => {
+    if (!isLoading) {
+      setPodcastsFetch(data);
+      setDisplayPodcasts(data);
+    }
+  }, [data, isLoading]);
+
   return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
 };
 
