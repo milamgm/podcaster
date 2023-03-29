592eec2ed0a50fc89700a05908996582812dee6a
milamgm <nocturnetheater@gmail.com>
2023-03-29 23:00:40 +0200

    feat(Home Page): add Home page logic and style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { ItemCard } from "../../common/utilities";
+import { useAppContext } from "../../application/context/AppContext";
+import { ItemCard, SearchField } from "../../common/utilities";
+import "./Home.scss";
 
 const Home = () => {
+  const { displayPodcasts } = useAppContext();
+
   return (
     <div className="home">
-      <ItemCard />
+      <div className="top_bar">
+        <SearchField />
+      </div>
+      <div className="items_list">
+        {displayPodcasts &&
+          displayPodcasts.map((podcast) => (
+            <ItemCard
+              key={podcast?.id.attributes["im:id"]}
+              id={podcast?.id.attributes["im:id"]}
+              title={podcast?.title.label}
+              author={podcast["im:artist"]?.label}
+              image={podcast["im:image"][1]?.label}
+            />
+          ))}
+      </div>
     </div>
   );
 };
