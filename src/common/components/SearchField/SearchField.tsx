211538bfd5e8e2d11e16ffc30fa836a26dc83410
milamgm <nocturnetheater@gmail.com>
2023-03-29 22:15:45 +0200

    feat(SearchField): add SearchField logic

--- src/common/components/SearchField/SearchField.tsx
+++ src/common/components/SearchField/SearchField.tsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
+import { useAppContext } from "../../../application/context/AppContext";
+import useSearch from "../../hooks/useSearch";
 import "./SearchField.scss";
 
 const SearchField = () => {
-  const [input, setInput] = useState("");
-
+  const { setDisplayPodcasts } = useAppContext();
+  const [query, setQuery] = useState("");
+  const filteredPodcasts = useSearch(query);
+  const podcastsQty = filteredPodcasts.length
+  useEffect(()=> {
+    setDisplayPodcasts(filteredPodcasts)
+  },[filteredPodcasts])
+  
   return (
     <div className="search_field">
-      <div className="badge">100</div>
+      <div className="badge">{podcastsQty}</div>
       <div className="input_field">
         <input
           type="text"
           placeholder="Filter podcasts..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
       </div>
     </div>
